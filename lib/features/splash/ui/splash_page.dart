de5fe1619901feda19aa866dfac11c9b31c4066f
Evgeny Glynskyi <evgeniuglinsky@gmail.com>
2026-02-23 10:18:32 +0200

    Added Splash Page and some dependencies

--- lib/features/splash/ui/splash_page.dart
+++ lib/features/splash/ui/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:obscuro_map/core/theme/dark_theme.dart';
 import 'package:obscuro_map/gen/assets.gen.dart';
 
 class SplashPage extends StatelessWidget {
@@ -7,8 +8,47 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: DarkTheme.darkThemeBackground,
       body: Center(
-        child: Assets.features.splash.appLogoPng.image(),
+        child: TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 0, end: 1),
+          duration: const Duration(milliseconds: 1800),
+          curve: Curves.easeInOut,
+          builder: (context, value, child) {
+            final logoAnimation = CurvedAnimation(
+              parent: AlwaysStoppedAnimation(value),
+              curve: const Interval(0, 0.6, curve: Curves.fastOutSlowIn),
+            ).value;
+
+            return Column(
+              mainAxisAlignment: .center,
+              children: [
+                Transform.translate(
+                  offset: Offset(0, (1 - logoAnimation) * 60),
+                  child: Opacity(
+                    opacity: logoAnimation,
+                    child: Assets.features.splash.appLogoPng.image(),
+                  ),
+                ),
+
+                const SizedBox(height: 98),
+
+                Opacity(
+                  opacity: CurvedAnimation(
+                    parent: AlwaysStoppedAnimation(value),
+                    curve: const Interval(0.7, 1.0, curve: Curves.linear),
+                  ).value,
+                  child: const CircularProgressIndicator(
+                    strokeWidth: 6,
+                    strokeAlign: 0,
+                    strokeCap: .round,
+                    color: DarkTheme.primary,
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
